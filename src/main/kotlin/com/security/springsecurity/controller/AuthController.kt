28e87d51ee2e2c135888b35744e8aeb6d8a627f2
Abid Hasan <abid.northsouth@gmail.com>
2019-10-23 16:24:58 +0600

    #1 Fixed user authentication without password

--- src/main/kotlin/com/security/springsecurity/controller/AuthController.kt
+++ src/main/kotlin/com/security/springsecurity/controller/AuthController.kt
@@ -1,7 +1,10 @@
 package com.security.springsecurity.controller
 
-import com.security.springsecurity.model.UserCredential
+import com.security.springsecurity.dto.UserCredentialDTO
 import com.security.springsecurity.utilities.TokenUtils
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/auth")
-class AuthController(val userDetailsService: UserDetailsService, val tokenUtils: TokenUtils) {
+class AuthController(val authenticationManager: AuthenticationManager, val userDetailsService: UserDetailsService, val tokenUtils: TokenUtils) {
 
     @PostMapping
-    fun authenticate(@RequestBody userCredential: UserCredential): String {
-        return tokenUtils.getToken(userDetailsService.loadUserByUsername(userCredential.username))
+    fun authenticate(@RequestBody userCredentialDTO: UserCredentialDTO): String {
+        SecurityContextHolder.getContext().authentication = authenticationManager.authenticate(UsernamePasswordAuthenticationToken(userCredentialDTO.username, userCredentialDTO.password))
+        return tokenUtils.getToken(userDetailsService.loadUserByUsername(userCredentialDTO.username))
     }
 }
