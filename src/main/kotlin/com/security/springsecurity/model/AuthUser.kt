c5ef5ef7e8d27d2049dfdffaeb073c9b375af058
Abid Hasan <abid.northsouth@gmail.com>
2019-10-23 16:27:03 +0600

    #4 Changed auth user to data class

--- src/main/kotlin/com/security/springsecurity/model/AuthUser.kt
+++ src/main/kotlin/com/security/springsecurity/model/AuthUser.kt
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-class AuthUser(
+data class AuthUser(
         @Id @GeneratedValue
         val id: Long?,
         val userName: String,
